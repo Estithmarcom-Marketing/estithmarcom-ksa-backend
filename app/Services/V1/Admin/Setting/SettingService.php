c158cea50514500989e692e2561016ed46ed10d8
Mhmud-Elsayed <houdaaaelsayed22222@gmail.com>
2026-05-31 14:43:45 +0300

    refactor(Setting Service) to be more readable

--- app/Services/V1/Admin/Setting/SettingService.php
+++ app/Services/V1/Admin/Setting/SettingService.php
@@ -15,24 +15,11 @@ class SettingService
     {
         $info = Setting::firstOrFail();
 
-        $phone = isset($data['phone'])
+        $data['phone'] = isset($data['phone'])
             ? str_replace(['+', ' ', '-'], '', $data['phone'])
             : $info->phone;
 
-        $info->update([
-            'name_ar' => $data['name_ar'] ?? $info->name_ar,
-            'name_en' => $data['name_en'] ?? $info->name_en,
-            'phone' => $phone,
-            'email' => $data['email'] ?? $info->email,
-            'address' => $data['address'] ?? $info->address,
-            'facebook' => $data['facebook'] ?? $info->facebook,
-            'x' => $data['x'] ?? $info->x,
-            'instagram' => $data['instagram'] ?? $info->instagram,
-            'linkedin' => $data['linkedin'] ?? $info->linkedin,
-            'whatsapp' => $data['whatsapp'] ?? $info->whatsapp,
-            'snapchat' => $data['snapchat'] ?? $info->snapchat,
-            'tiktok' => $data['tiktok'] ?? $info->tiktok,
-        ]);
+        $info->update($data);
 
         return $info->refresh();
     }
